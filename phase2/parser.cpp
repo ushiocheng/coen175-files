28c3f2c99b4900ce05f2bed4e9af91c1452f492e
cheng zhang <czhang9@linux10609.dc.engr.scu.edu>
2023-01-23 15:56:20 -0800

    [Lab 2] not working full parser

--- phase2/parser.cpp
+++ phase2/parser.cpp
@@ -7,9 +7,8 @@
 
 using namespace std;
 
-int lookahead;
-
-int main() {
+int main()
+{
     lookahead = yylex();
     start();
     // int token;
@@ -31,6 +30,161 @@ int main() {
     // }
 }
 
+void prettyPrint(int tmp)
+{
+    if (tmp < 256)
+    {
+        cout << (char)tmp;
+    }
+    else
+    {
+        switch (tmp)
+        {
+        case AUTO:
+            cout << "AUTO";
+            break;
+        case BREAK:
+            cout << "BREAK";
+            break;
+        case CASE:
+            cout << "CASE";
+            break;
+        case CHAR:
+            cout << "CHAR";
+            break;
+        case CONST:
+            cout << "CONST";
+            break;
+        case CONTINUE:
+            cout << "CONTINUE";
+            break;
+        case DEFAULT:
+            cout << "DEFAULT";
+            break;
+        case DO:
+            cout << "DO";
+            break;
+        case DOUBLE:
+            cout << "DOUBLE";
+            break;
+        case ELSE:
+            cout << "ELSE";
+            break;
+        case ENUM:
+            cout << "ENUM";
+            break;
+        case EXTERN:
+            cout << "EXTERN";
+            break;
+        case FLOAT:
+            cout << "FLOAT";
+            break;
+        case FOR:
+            cout << "FOR";
+            break;
+        case GOTO:
+            cout << "GOTO";
+            break;
+        case IF:
+            cout << "IF";
+            break;
+        case INT:
+            cout << "INT";
+            break;
+        case LONG:
+            cout << "LONG";
+            break;
+        case REGISTER:
+            cout << "REGISTER";
+            break;
+        case RETURN:
+            cout << "RETURN";
+            break;
+        case SHORT:
+            cout << "SHORT";
+            break;
+        case SIGNED:
+            cout << "SIGNED";
+            break;
+        case SIZEOF:
+            cout << "SIZEOF";
+            break;
+        case STATIC:
+            cout << "STATIC";
+            break;
+        case STRUCT:
+            cout << "STRUCT";
+            break;
+        case SWITCH:
+            cout << "SWITCH";
+            break;
+        case TYPEDEF:
+            cout << "TYPEDEF";
+            break;
+        case UNION:
+            cout << "UNION";
+            break;
+        case UNSIGNED:
+            cout << "UNSIGNED";
+            break;
+        case VOID:
+            cout << "VOID";
+            break;
+        case VOLATILE:
+            cout << "VOLATILE";
+            break;
+        case WHILE:
+            cout << "WHILE";
+            break;
+        case OP_OR:
+            cout << "OP_OR";
+            break;
+        case OP_AND:
+            cout << "OP_AND";
+            break;
+        case OP_EQ:
+            cout << "OP_EQ";
+            break;
+        case OP_NE:
+            cout << "OP_NE";
+            break;
+        case OP_LE:
+            cout << "OP_LE";
+            break;
+        case OP_GE:
+            cout << "OP_GE";
+            break;
+        case OP_INC:
+            cout << "OP_INC";
+            break;
+        case OP_DEC:
+            cout << "OP_DEC";
+            break;
+        case OP_ARROW:
+            cout << "OP_ARROW";
+            break;
+        case STRING:
+            cout << "STRING";
+            break;
+        case CHARACTER:
+            cout << "CHARACTER";
+            break;
+        case ID:
+            cout << "ID";
+            break;
+        case NUM:
+            cout << "NUM";
+            break;
+        case Done:
+            cout << "Done";
+            break;
+        default:
+            cout << "????";
+            break;
+        }
+    }
+}
+
 /**
  * Match token and advance lookahead
  * @param token_type type of token, defined in tokens.h
@@ -38,48 +192,70 @@ int main() {
  * message
  * @remark when no argument are given, this simple advances the lookahead
  */
-void match(int token_type = lookahead, string caller = "") {
-    if (lookahead == token_type) {
+void match(int token_type)
+{
+    if (lookahead == token_type)
+    {
         lookahead = yylex();
         // TODO: pass token content back to caller for sematics?
-    } else {
+    }
+    else
+    {
         // error
         // TODO: fix this
-        cerr << "[ERROR] Parsing error at line " << yylineno() << ", expecting "
-             << token_type << " and found " << lookahead << endl;
+        cout << "[ERROR] Parsing error at line " << yylineno << ", expecting ";
+        prettyPrint(token_type);
+        cout << " and found ";
+        prettyPrint(lookahead);
+        cout << endl;
+        exit(0);
         // report("[ERROR] Parsing error at line %d, expecting %d and found
         // %d",yylineno(),token_type,lookahead);
     }
 }
 
-void start() { translation_unit(); }
+void start()
+{
+    // translation_unit();
+    // TODO: remove debug stuff
+    expression();
+}
 
 // translation-unit -> ε
 //                  | global-declaration translation-unit
 //                  | function-definition translation-unit
-void translation_unit() {
-    while (lookahead != Done) {
+void translation_unit()
+{
+    while (lookahead != Done)
+    {
         // global declaration or function definition
         // take up 'specifier pointer id'
         specifier();
         pointers();
         match(ID);
-        if (lookahead == '(') {
+        if (lookahead == '(')
+        {
             match();
-            if (lookahead == ')') {
+            if (lookahead == ')')
+            {
                 match();
                 // This is a global-declaration
                 rest_of_global_declarator_list();
-            } else {
+            }
+            else
+            {
                 // This is a function-definition
                 rest_of_function_definition();
             }
-        } else {
+        }
+        else
+        {
             // This is a global-declaration
-            if (lookahead == '[') {
+            if (lookahead == '[')
+            {
                 match();
                 match(NUM);
-                match(']');
+                match(OP_R_BRACKET);
             }
             rest_of_global_declarator_list();
         }
@@ -90,34 +266,42 @@ void translation_unit() {
 // global-declarator-list -> global-declarator
 //!                       | global-declarator **, global-declarator-list**
 
-void rest_of_global_declarator_list() {
-    while (lookahead == ',') {
+void rest_of_global_declarator_list()
+{
+    while (lookahead == ',')
+    {
         match();
         global_declarator();
     }
-    match(';');
+    match(OP_SC);
 }
 
 // global-declarator -> pointers id
 //                    | pointers id ( )
 //                    | pointers id [ num ]
-void global_declarator() {
+void global_declarator()
+{
     pointers();
     match(ID);
-    if (lookahead == '(') {
+    if (lookahead == '(')
+    {
         match();
-        match(')');
-    } else if (lookahead == '[') {
+        match(OP_R_PARENT);
+    }
+    else if (lookahead == '[')
+    {
         match();
         match(NUM);
-        match(']');
+        match(OP_R_BRACKET);
     }
 }
 
 // pointers -> ε
 // | * pointers
-void pointers() {
-    while (lookahead == '*') {
+void pointers()
+{
+    while (lookahead == '*')
+    {
         match();
     }
 }
@@ -126,8 +310,10 @@ void pointers() {
 // | char
 // | long
 // | void
-void specifier() {
-    if (lh_is_specifier()) {
+void specifier()
+{
+    if (lh_is_specifier())
+    {
         match();
         return;
     }
@@ -140,32 +326,39 @@ void specifier() {
  * ! This is NOT part of the syntax tree
  * check to see if lookahead matches one of the specifiers
  */
-inline bool lh_is_specifier() {
+inline bool lh_is_specifier()
+{
     return (lookahead == INT) || (lookahead == CHAR) || (lookahead == LONG) ||
            (lookahead == VOID);
 }
 
 // function-definition -> specifier pointers id (
 //! parameters ) { declarations statements }
-void rest_of_function_definition() {
+void rest_of_function_definition()
+{
     parameters();
-    match(')');
-    match('{');
+    match(OP_R_PARENT);
+    match(OP_L_BRACE);
     declarations();
     statements();
-    match('}');
+    match(OP_R_BRACE);
 }
 
 // parameters -> void
 //      | parameter-list
 // parameter-list -> parameter
 //      | parameter , parameter-list
-void parameters() {
-    if (lookahead == VOID) {
+void parameters()
+{
+    if (lookahead == VOID)
+    {
         match();
-    } else {
+    }
+    else
+    {
         parameter();
-        while (lookahead == ',') {
+        while (lookahead == ',')
+        {
             match();
             parameter();
         }
@@ -173,7 +366,8 @@ void parameters() {
 }
 
 // parameter -> specifier pointers id
-void parameter() {
+void parameter()
+{
     specifier();
     pointers();
     match(ID);
@@ -182,21 +376,25 @@ void parameter() {
 // declarations -> ε
 //      | declaration declarations
 // declaration -> specifier declarator-list ;
-void declarations() {
+void declarations()
+{
     //! declarations and statements are competing for tokens, but only
     //! declarations start with specifiers
-    while (lh_is_specifier()) {
+    while (lh_is_specifier())
+    {
         specifier();
         declarator_list();
-        match(';')
+        match(OP_SC);
     }
 }
 
 // declarator-list -> declarator
 //      | declarator , declarator-list
-void declarator_list() {
+void declarator_list()
+{
     declarator();
-    while (lookahead == ',') {
+    while (lookahead == ',')
+    {
         match();
         declarator();
     }
@@ -204,20 +402,24 @@ void declarator_list() {
 
 // declarator -> pointers id
 //      | pointers id [ num ]
-void declarator() {
+void declarator()
+{
     pointers();
     match(ID);
-    if (lookahead == '[') {
+    if (lookahead == '[')
+    {
         match();
         match(NUM);
-        match(']');
+        match(OP_R_BRACKET);
     }
 }
 
 // statements -> ε
 // | statement statements
-void statements() {
-    while (lookahead != '}') {
+void statements()
+{
+    while (lookahead != '}')
+    {
         //! Statement cannot start with '}' and must be followed by '}'. So this
         //! determines if there is more statement
         statement();
@@ -231,47 +433,70 @@ void statements() {
 // | if ( expression ) statement
 // | if ( expression ) statement else statement
 // | assignment ;
-void statement() {
-    if (lookahead == '{') {
+void statement()
+{
+    if (lookahead == '{')
+    {
         match();
         declarations();
         statements();
-        match('}');
-    } else if (lookahead == RETURN) {
+        match(OP_R_BRACE);
+    }
+    else if (lookahead == RETURN)
+    {
         match();
         expression();
-        match(';');
-    } else if (lookahead == WHILE) {
+        match(OP_SC);
+    }
+    else if (lookahead == WHILE)
+    {
         // | while ( expression ) statement
         match();
-        match('(');
+        match(OP_L_PARENT);
         expression();
-        match(')');
+        match(OP_R_PARENT);
         statement();
-    } else if (lookahead == WHILE) {
+    }
+    else if (lookahead == WHILE)
+    {
         // | for ( assignment ; expression ; assignment ) statement
-        
+
         // TODO: continue here
-    } else if (lookahead == WHILE) {
+    }
+    else if (lookahead == WHILE)
+    {
         // | if ( expression ) statement
         // | if ( expression ) statement else statement
-    } else {
+    }
+    else
+    {
         // | assignment ;
     }
 }
 
 // assignment -> expression = expression
-// | expression
-void assignment() {
+//            | expression
+void assignment()
+{
     expression();
-    if (lookahead == '=') {
+    if (lookahead == '=')
+    {
         match();
         expression();
     }
 }
 
 // expression-list -> expression
-// | expression , expression-list
+//                 | expression , expression-list
+void expression_list()
+{
+    expression();
+    while (lookahead == ',')
+    {
+        match();
+        expression();
+    }
+}
 
 // expression -> expression || expression
 // | expression && expression
@@ -299,3 +524,223 @@ void assignment() {
 // | string
 // | character
 // | ( expression )
+void expression()
+{
+    expression_level_1();
+}
+
+void expression_level_1()
+{
+    expression_level_2();
+    while (lookahead == OP_OR)
+    {
+        match();
+        expression_level_2();
+        cout << "or" << endl;
+    }
+}
+
+void expression_level_2()
+{
+    expression_level_3();
+    while (lookahead == OP_AND)
+    {
+        match();
+        expression_level_3();
+        cout << "and" << endl;
+    }
+}
+
+void expression_level_3()
+{
+    expression_level_4();
+    while (lookahead == OP_EQ || lookahead == OP_NE)
+    {
+        int tmp = lookahead;
+        match();
+        expression_level_4();
+        switch (tmp)
+        {
+        case OP_EQ:
+            cout << "eql" << endl;
+            break;
+
+        default: // OP_NE
+            cout << "neq" << endl;
+            break;
+        }
+    }
+}
+
+void expression_level_4()
+{
+    expression_level_5();
+    while (
+        lookahead == OP_LT ||
+        lookahead == OP_GT ||
+        lookahead == OP_LE ||
+        lookahead == OP_GE)
+    {
+        int tmp = lookahead;
+        match();
+        expression_level_5();
+        switch (tmp)
+        {
+        case OP_LT:
+            cout << "ltn" << endl;
+            break;
+        case OP_GT:
+            cout << "gtn" << endl;
+            break;
+        case OP_LE:
+            cout << "leq" << endl;
+            break;
+
+        default: // OP_GE
+            cout << "geq" << endl;
+            break;
+        }
+    }
+}
+
+void expression_level_5()
+{
+    expression_level_6();
+    while (
+        lookahead == '+' ||
+        lookahead == '-')
+    {
+        int tmp = lookahead;
+        match();
+        expression_level_6();
+        switch (tmp)
+        {
+        case '+':
+            cout << "add" << endl;
+            break;
+
+        default:
+            cout << "sub" << endl;
+            break;
+        }
+    }
+}
+
+void expression_level_6()
+{
+    expression_level_7();
+    while (
+        lookahead == '*' ||
+        lookahead == '/' ||
+        lookahead == '%')
+    {
+        int tmp = lookahead;
+        match();
+        expression_level_7();
+        switch (tmp)
+        {
+        case '*':
+            cout << "mul" << endl;
+            break;
+        case '/':
+            cout << "div" << endl;
+            break;
+        default:
+            cout << "rem" << endl;
+            break;
+        }
+    }
+}
+
+void expression_level_7()
+{
+    int tmp = -1;
+    if (
+        lookahead == '&' ||
+        lookahead == '*' ||
+        lookahead == '!' ||
+        lookahead == '-' ||
+        lookahead == SIZEOF)
+    {
+        tmp = lookahead;
+        match();
+        expression_level_7();
+    }
+    expression_level_8();
+    switch (tmp)
+    {
+    case '&':
+        cout << "addr" << endl;
+        break;
+    case '*':
+        cout << "deref" << endl;
+        break;
+    case '!':
+        cout << "not" << endl;
+        break;
+    case '-':
+        cout << "neg" << endl;
+        break;
+    case SIZEOF:
+        cout << "sizeof" << endl;
+        break;
+    default:
+        break;
+    }
+}
+
+void expression_level_8()
+{
+    expression_term();
+    while (lookahead == '[')
+    {
+        match();
+        expression();
+        match(OP_R_BRACKET);
+        cout << "index" << endl;
+    }
+}
+
+// | id ( expression-list )
+// | id ( )
+// | id
+// | num
+// | string
+// | character
+// | ( expression )
+void expression_term()
+{
+    if (lookahead == ID)
+    {
+        match();
+        if (lookahead == '(')
+        {
+            match();
+            if (lookahead == ')')
+            {
+                match();
+                return;
+            }
+            expression_list();
+            match(OP_R_PARENT);
+        }
+    }
+    else if (lookahead == NUM)
+    {
+        match();
+    }
+    else if (lookahead == STRING)
+    {
+        match();
+    }
+    else if (lookahead == CHARACTER)
+    {
+        match();
+    }
+    else
+    { //! MUST be '('
+        match(OP_L_PARENT);
+        expression();
+        match(OP_R_PARENT);
+    }
+}
