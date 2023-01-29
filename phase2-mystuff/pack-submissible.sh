519bf3184db5b0d1547c1472b67e117d722ac89c
Ushio Cheng <ushio.cheng@gmail.com>
2023-01-29 15:59:42 -0800

    [phase 2] fix pack-submissible & add more logging

--- phase2-mystuff/pack-submissible.sh
+++ phase2-mystuff/pack-submissible.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 cd ~/coen175-files/
-tar -cf p2-examples.tar phase2-mystuff/examples
-tar -cf phase2.tar phase2
-sh phase2-mystuff/checksub.sh phase2.tar p2-examples.tar
+printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
+
+tar -cf phase2-mystuff/p2-examples.tar phase2-mystuff/examples \
+&& printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/p2-examples.tar\n" \
+|| printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/p2-examples.tar\n";
+
+tar -cf phase2-mystuff/phase2.tar phase2 \
+&& printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/phase2.tar\n" \
+|| printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/phase2.tar\n";
+
+cd phase2-mystuff/
+printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
+
+printf "\x1b[102;30m[INFO]\x1b[0m calling checksub.sh\n"
+sh checksub.sh phase2.tar p2-examples.tar
+
+printf "\x1b[102;30m[INFO]\x1b[0m Removing p2-examples.tar\n"
 rm p2-examples.tar
