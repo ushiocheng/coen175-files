1220e04a29cb7946047994822bba26cd08181437
Ushio Cheng <ushio.cheng@gmail.com>
2023-01-29 15:59:42 -0800

    [phase 2] fix pack-submissible & add more logging

--- phase2-mystuff/pack-submissible.sh
+++ phase2-mystuff/pack-submissible.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-cd ~/coen175-files/
+cd ~/coen175-files/phase2-mystuff/
 printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
 
-tar -cf phase2-mystuff/p2-examples.tar phase2-mystuff/examples \
+tar -cf p2-examples.tar examples \
 && printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/p2-examples.tar\n" \
 || printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/p2-examples.tar\n";
 
-tar -cf phase2-mystuff/phase2.tar phase2 \
+tar -cf phase2.tar ../phase2 \
 && printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/phase2.tar\n" \
 || printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/phase2.tar\n";
-
-cd phase2-mystuff/
-printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
 
 printf "\x1b[102;30m[INFO]\x1b[0m calling checksub.sh\n"
 sh checksub.sh phase2.tar p2-examples.tar
