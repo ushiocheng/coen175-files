c26a1d24f6ec856eeb048c211b1af69da7a1d366
Ushio Cheng <ushio.cheng@gmail.com>
2023-01-29 17:22:46 -0800

    [phase 2] fix test case: output tag & break up OP_DEC

--- phase2-mystuff/examples/my_test_05.c
+++ phase2-mystuff/examples/my_test_05.c
@@ -1,3 +1,3 @@
 void * func1(void) {
-    return **********u[1][2]+i[00]-r*e/h+f-k*d/(sizeof b +c*v*s/h/y/v+b+i+u+s)-y-h-i-----------s-j-!-&&&*-***f*s*d+---'a';
+    return **********u[1][2]+i[00]-r*e/h+f-k*d/(sizeof b +c*v*s/h/y/v+b+i+u+s)-y-h-i- - - - - - - - - - -s-j-!-&&&*-***f*s*d+ - - -'a';
 }
