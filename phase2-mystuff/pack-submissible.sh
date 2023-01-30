696a761978129c27c167f9aaf4c8cd8c56705dd5
Ushio Cheng <ushio.cheng@gmail.com>
2023-01-29 16:12:35 -0800

    [phase 2] Improve pack-submissible.sh

--- phase2-mystuff/pack-submissible.sh
+++ phase2-mystuff/pack-submissible.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+if [[ -e ~/coen175-files/phase2-mystuff/ ]]; then
+    cd ~/coen175-files/
+    printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
+else
+    printf "\x1b[33;41m[ERROR]\x1b[0m Working Dir does not exist\n"
+    exit 1
+fi
+
+tar -cf phase2-mystuff/phase2.tar phase2 &&
+    printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/phase2.tar\n" ||
+    printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/phase2.tar\n"
+
 cd ~/coen175-files/phase2-mystuff/
-printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
 
-tar -cf p2-examples.tar examples \
-&& printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/p2-examples.tar\n" \
-|| printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/p2-examples.tar\n";
-
-tar -cf phase2.tar ../phase2 \
-&& printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/phase2.tar\n" \
-|| printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/phase2.tar\n";
+tar -cf p2-examples.tar examples &&
+    printf "\x1b[102;30m[INFO]\x1b[0m packed phase2-mystuff/p2-examples.tar\n" ||
+    printf "\x1b[33;41m[ERROR]\x1b[0m Failed to pack phase2-mystuff/p2-examples.tar\n"
 
 printf "\x1b[102;30m[INFO]\x1b[0m calling checksub.sh\n"
 sh checksub.sh phase2.tar p2-examples.tar
