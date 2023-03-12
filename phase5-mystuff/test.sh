c609d7ce068dcc13503da0cd267da3a85f2c66bd
ushiocheng <32724585+ushiocheng@users.noreply.github.com>
2023-03-12 16:31:38 -0700

    fix shell print color

--- phase5-mystuff/test.sh
+++ phase5-mystuff/test.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/zsh
+
+infoLabel=$(printf "\x1b[102;30m[INFO]\x1b[0m")
+warnLabel=$(printf "\x1b[103;30m[WARN]\x1b[0m")
+errorLabel=$(printf "\x1b[33;41m[ERROR]\x1b[0m")
 
 if [ -z $1 ]; then
-    # printf "\x1b[33;41m[ERROR]\x1b[0m Usage: $0 <path-to-coen175-workspace>\n"
+    # printf "$errorLabel Usage: $0 <path-to-coen175-workspace>\n"
     # exit 1
     cd ..
-    printf "\x1b[103;30m[WARN]\x1b[0m Inferring workspace base dir => $(pwd)\n"
+    printf "$warnLabel Inferring workspace base dir => $(pwd)\n"
 else
     cd $1
 fi
@@ -13,13 +17,13 @@ basedir=$(pwd)
 
 if [ -e $basedir/phase5-mystuff/ ]; then
     cd $basedir/phase5
-    printf "\x1b[102;30m[INFO]\x1b[0m working in $(pwd)\n"
+    printf "$infoLabel working in $(pwd)\n"
 else
-    printf "\x1b[33;41m[ERROR]\x1b[0m Working Dir does not exist\n"
+    printf "$errorLabel Working Dir does not exist\n"
     exit 1
 fi
 
-printf "\x1b[102;30m[INFO]\x1b[0m Building phase5\n"
+printf "$infoLabel Building phase5\n"
 make clean
 make
 
@@ -30,7 +34,7 @@ cd examples
 
 for f in *-lib.c; do
     BASE=$(basename $f -lib.c)
-    printf "\x1b[102;30m[INFO]\x1b[0m Testing $BASE ... "
+    printf "$infoLabel Testing $BASE ... "
     ulimit -t 1
     ../../phase5/scc \
         <$BASE.c \
@@ -44,7 +48,7 @@ for f in *-lib.c; do
         printf "ok\n" ||
         (
             printf "failed\n"
-            printf "\x1b[33;41m[ERROR]\x1b[0m Test case $BASE failed!\n"
+            printf "$errorLabel Test case $BASE failed!\n"
             cp $BASE.out ../test-output/$BASE.runtime.out.expected
             diff $BASE.out ../test-output/$BASE.runtime.out > ../test-output/$BASE.runtime.out.diff
         )
